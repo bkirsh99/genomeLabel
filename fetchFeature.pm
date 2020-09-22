2c8bcb99213acc0d74aac96587bf8483f50cf65c
bkirsh99 <42989034+bkirsh99@users.noreply.github.com>
2020-09-22 15:23:14 -0700

    Update fetchFeature.pm

--- fetchFeature.pm
+++ fetchFeature.pm
@@ -21,7 +21,7 @@ sub new {
     my ($class, $args) = @_;
     my $self = bless {
         name => $args->{name},
-        url => $args->{url} || undef, 
+        url => $args->{url} || undef,
         hub => $args->{hub} || undef,
         trackName => $args->{trackName} || undef,
         chrom => $chr,
@@ -29,9 +29,11 @@ sub new {
         chromEnd => $end,
         genome => $args->{genome} || 'hg38',
         fixed => $args->{fixed} || 0,
-	}, $class;
-	
-	$self->load unless $self->{fixed};
+        }, $class;
+
+        #$self->load unless $self->{fixed};
+        $self->load;
+        $self->make_AoH unless $self->{fixed};
 
    return $self;
 }
@@ -52,23 +54,23 @@ sub make_AoH{
                 my $text = decode_json( $data );
                 @fileDump = @{ $text->{ $self->{trackName} } };
         } else {
-		unless ($self->{fixed}) {
-			my $parser = Bio::ToolBox::parser::gff->new(
-                        	file => $self->load,
-                        	#do_gene => 0, 
-                        	#do_exon => 0, 
-                        	do_cds => 1,
-                        	do_utr => 1,
-                        	do_codon => 1,
-                	) or die "Unable to open \$parser->{file}, please ensure '.gff' or '.gtf' extension.\n";
+                unless ($self->{fixed}) {
+                        my $parser = Bio::ToolBox::parser::gff->new(
+                                file => $self->load,
+                                #do_gene => 0,
+                                #do_exon => 0,
+                                do_cds => 1,
+                                do_utr => 1,
+                                do_codon => 1,
+                        ) or die "Unable to open \$parser->{file}, please ensure '.gff' or '.gtf' extension.\n";
 
-                	while (my $feature = $parser->next_feature() ) {
-                	#each $feature is a SeqFeature object
-                        	my %hash = gtfCols(@$feature);
-                        	push @fileDump, \%hash;
-                	}
-        	}
-	} 
+                        while (my $feature = $parser->next_feature() ) {
+                        #each $feature is a SeqFeature object
+                                my %hash = gtfCols(@$feature);
+                                push @fileDump, \%hash;
+                        }
+                }
+        }
         $self->{array} = \@fileDump;
 }
 
@@ -78,44 +80,44 @@ sub load{
 
         if(defined $self->{url}){
                 my $file = (split '/', $self->{url})[-1];
-		system "wget -O- -q $self->{url} > $file" if (!-f $file);
+                system "wget -O- -q $self->{url} > $file" if (!-f $file);
 
-		unless ($self->{fixed}){
-			$tmp .= ".gtf.gz";
-			$command = "gunzip -c $file | awk -F \"\\t\" 'BEGIN{OFS=\"\\t\";} ((\$9~/NM/ || \$9~/NR/) && \$1==\"$self->{chrom}\" && ((\$5 >= $self->{chromStart} && \$5 <= $self->{chromEnd}) || (\$4 >= $self->{chromStart} && \$4 <= $self->{chromEnd})))' | gzip > $tmp";
-		}
-        
-	} else {
-		if(defined $self->{hub}){
-                 	$command = "wget -O- -q 'http://api.genome.ucsc.edu/getData/track?hubUrl=$self->{hub};genome=$self->{genome};track=$self->{trackName};chrom=$self->{chrom};start=$self->{chromStart};end=$self->{chromEnd}' > $tmp";
-        	} 
-		else {
-	        	 $command = "wget -O- -q 'http://api.genome.ucsc.edu/getData/track?genome=$self->{genome};track=$self->{trackName};chrom=$self->{chrom};start=$self->{chromStart};end=$self->{chromEnd}' > $tmp";
-        	} 
-	}
+                unless ($self->{fixed}){
+                        $tmp .= ".gtf.gz";
+                        $command = "gunzip -c $file | awk -F \"\\t\" 'BEGIN{OFS=\"\\t\";} ((\$9~/NM/ || \$9~/NR/) && \$1==\"$self->{chrom}\" && ((\$5 >= $self->{chromStart} && \$5 <= $self->{chromEnd}) || (\$4 >= $self->{chromStart} && \$4 <= $self->{chromEnd})))' | gzip > $tmp";
+                }
 
-       	if (defined $command){
-		system($command);
-	}
+        } else {
+                if(defined $self->{hub}){
+                        $command = "wget -O- -q 'http://api.genome.ucsc.edu/getData/track?hubUrl=$self->{hub};genome=$self->{genome};track=$self->{trackName};chrom=$self->{chrom};start=$self->{chromStart};end=$self->{chromEnd}' > $tmp";
+                }
+                else {
+                         $command = "wget -O- -q 'http://api.genome.ucsc.edu/getData/track?genome=$self->{genome};track=$self->{trackName};chrom=$self->{chrom};start=$self->{chromStart};end=$self->{chromEnd}' > $tmp";
+                }
+        }
 
-	return $tmp;
+        if (defined $command){
+                system($command);
+        }
+
+        return $tmp;
 
 }
 
 sub gtfCols{
         my @colVals = @_;
         my %arrayVals = ( SEQID => 0,
-	        START => 1,
-        	STOP  => 2,
-        	STRND => 3,
-        	NAME  => 4,
-        	ID    => 5,
-        	TYPE  => 6,
-        	SRC   => 7,
-        	SCORE => 8,
-        	PHASE => 9,
-        	ATTRB => 10,
-        	SUBF => 11 );
+                START => 1,
+                STOP  => 2,
+                STRND => 3,
+                NAME  => 4,
+                ID    => 5,
+                TYPE  => 6,
+                SRC   => 7,
+                SCORE => 8,
+                PHASE => 9,
+                ATTRB => 10,
+                SUBF => 11 );
 
         my %hash;
         foreach my $key (keys %arrayVals){
